2d6cae96bf16c98af662ccf7b74e5d8a6a87cdb7
tomazvila <tomazvila@outlook.com>
2022-08-01 11:57:14 +0000

    Remove not used anymore TimeNft tn convert fn

--- src/Common/Utils.hs
+++ src/Common/Utils.hs
@@ -13,18 +13,6 @@ info = scriptContextTxInfo
 hasUTxO :: TxOutRef -> ScriptContext -> Bool
 hasUTxO utxo ctx = traceIfFalse "No minting policy specified utxo found" $ any (\i -> txInInfoOutRef i == utxo) $ txInfoInputs (info ctx)
 
-{-# INLINEABLE intToByteString #-}
-intToByteString :: Integer -> BuiltinByteString
-intToByteString x = if x `B.divideInteger` 10 == 0 then digitToByteString x
-  else
-    B.appendByteString (intToByteString (x `B.divideInteger` 10)) (digitToByteString (x `B.modInteger` 10))
-       where
-         digitToByteString :: Integer -> BuiltinByteString
-         digitToByteString d = B.consByteString (d `B.addInteger` asciZero) B.emptyByteString
-
-         asciZero :: Integer
-         asciZero = 48
-
 {-# INLINEABLE range #-}
 range :: ScriptContext -> POSIXTimeRange
 range ctx = txInfoValidRange (info ctx)
